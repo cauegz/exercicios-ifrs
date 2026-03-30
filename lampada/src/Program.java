5e034401860ebe6847d1349f8398766595a63413
Cauê Geraldi Zanardi <cauegeraldizanardi@gmail.com>
2026-03-30 16:49:25 -0300

    Melhoria do exercício da lâmpada

--- lampada/src/Program.java
+++ lampada/src/Program.java
@@ -2,12 +2,23 @@ package src;
 
 import entities.Lampada;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        Lampada lamp = new Lampada(true);
+        Scanner sc = new Scanner(System.in);
 
-        lamp.interruptor();
+        System.out.print("Estado da lâmpada (0 = desligada | 1 = ligada): ");
+        int aux = sc.nextInt();
+        boolean ligada = (aux != 0);
+
+        System.out.print("Watts (-1 = valor padrão): ");
+        int watts = sc.nextInt();
+
+        Lampada lamp = new Lampada(ligada, watts);
 
         lamp.exibe();
+
+        sc.close();
     }
 }
